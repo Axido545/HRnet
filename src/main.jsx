c557f796d0768d86cc77a60a2a19d97c74c58e2a
axido545 <axido545@gmail.com>
2024-01-21 11:34:56 +0100

    add home page form

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/home/home.jsx'
-import './index.css'
 import {
   createBrowserRouter,
   RouterProvider,
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,5 +1,54 @@
+import { NavLink } from "react-router-dom"
 import "./home.css"
 
 export default function Home() {
-    return <>Home page HRnet</>
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        alert("submit")
+    }
+
+    return <> <h1 className="title">HRnet</h1>
+        <div className="container">
+            <NavLink>View Current Employees</NavLink>
+            <h2>Create Employee</h2>
+            <form onSubmit={handleSubmit}>
+                <label htmlFor="first-name">First Name</label>
+                <input type="text" id="first-name" />
+
+                <label htmlFor="last-name">Last Name</label>
+                <input type="text" id="last-name" />
+
+                <label htmlFor="date-of-birth">Date of Birth</label>
+                <input id="date-of-birth" type="text" />
+
+                <label htmlFor="start-date">Start Date</label>
+                <input id="start-date" type="text" />
+
+                <fieldset>
+                    <legend>Adress</legend>
+                    <label htmlFor="street">Street</label>
+                    <input id="street" type="text" />
+
+                    <label htmlFor="city">City</label>
+                    <input id="city" type="text" />
+
+                    <label htmlFor="state">State</label>
+                    <select name="state" id="state"></select>
+
+                    <label htmlFor="zip-code">Zip Code</label>
+                </fieldset>
+                <label htmlFor="department">Department</label>
+                <select name="department" id="department">
+                    <option>Sales</option>
+                    <option>Marketing</option>
+                    <option>Engineering</option>
+                    <option>Human Resources</option>
+                    <option>Legal</option>
+                </select>
+                <input className="saveBtn" type="submit" value="Save" />
+                <div id="confirmation" className="modal">Employee Created!</div>
+            </form >
+        </div >
+    </>
 }

bbaec7100e94f8514d38339cef94c5187ab5ad2d
axido545 <axido545@gmail.com>
2024-01-24 14:21:40 +0100

    desactive redux modif form

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -8,6 +8,14 @@ import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import MenuItem from '@mui/material/MenuItem';
 import Box from '@mui/material/Box';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { states } from "../../utils/state"
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+
 
 
 
@@ -22,37 +30,60 @@ export default function form() {
 
     return <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
         <Box className="formField" marginBottom={2}>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <FormGroup>
+                    <TextField label="First Name" id="first-name" variant="outlined" />
+                    <TextField label="Last Name" id="last-name" variant="outlined" />
+                    <DatePicker label="Date of Birth" id="date-of-birth" variant="outlined" />
+                    <DatePicker label="Start Date" id="start-date" variant="outlined" />
+                    <Card variant="outlined">
+                        <CardContent>
+                            <Typography variant="h6" gutterBottom>
+                                Address
+                            </Typography>
 
-            <FormGroup>
-                <TextField label="First Name" id="first-name" variant="outlined" />
-                <TextField label="Last Name" id="last-name" variant="outlined" />
-                <TextField label="Date of Birth" id="date-of-birth" variant="outlined" />
-                <TextField label="Start Date" id="start-date" variant="outlined" />
-                <Card variant="outlined">
-                    <CardContent>
-                        <Typography variant="h6" gutterBottom>
-                            Address
-                        </Typography>
+                            <TextField label="Street" id="street" variant="outlined" />
+                            <TextField label="City" id="city" variant="outlined" />
+                            <FormControl fullWidth>
+                                <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                                <Select
+                                    labelId="demo-simple-select-label"
+                                    id="demo-simple-select"
+                                    label="Age"
+                                >
+                                    {
+                                        //map sur tableau state
+                                    }
+                                </Select>
+                            </FormControl>
 
-                        <TextField label="Street" id="street" variant="outlined" />
-                        <TextField label="City" id="city" variant="outlined" />
-                        <TextField label="State" id="state" variant="outlined" />
-                        <TextField label="Zip Code" id="zip-code" variant="outlined" />
-                    </CardContent>
+                            <TextField label="Zip Code" id="zip-code" variant="outlined" />
+                        </CardContent>
 
-                </Card>
+                    </Card>
+                    <FormControl fullWidth>
+                        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                        <Select
+                            labelId="demo-simple-select-label"
+                            id="demo-simple-select"
+                            label="Age"
+                        >
+                            <MenuItem value="Sales">Sales</MenuItem>
+                            <MenuItem value="Marketing">Marketing</MenuItem>
+                            <MenuItem value="Engineering">Engineering</MenuItem>
+                            <MenuItem value="Human Resources">Human Resources</MenuItem>
+                            <MenuItem value="Legal">Legal</MenuItem>
+                        </Select>
+                    </FormControl>
 
-                <TextField select label="Department" id="department" variant="outlined" fullWidth>
-                    <MenuItem value="Sales">Sales</MenuItem>
-                    <MenuItem value="Marketing">Marketing</MenuItem>
-                    <MenuItem value="Engineering">Engineering</MenuItem>
-                    <MenuItem value="Human Resources">Human Resources</MenuItem>
-                    <MenuItem value="Legal">Legal</MenuItem>
-                </TextField>
-                <Button className="saveBtn" variant="contained" color="primary" type="submit">
-                    Save
-                </Button><Modal />
-            </FormGroup>
+
+
+                    <Button className="saveBtn" variant="contained" color="primary" type="submit">
+                        Save
+                    </Button>
+                    <Modal />
+                </FormGroup>
+            </LocalizationProvider>
         </Box>
 
     </form >

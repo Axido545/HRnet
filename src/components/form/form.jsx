1b03487d669133b5f9813b98ed72ae12c38005b9
axido545 <axido545@gmail.com>
2024-01-28 23:49:05 +0100

    form avec erreur console (state et departement)

--- src/components/form/form.jsx
+++ src/components/form/form.jsx
@@ -15,59 +15,80 @@ import { states } from "../../utils/state"
 import InputLabel from '@mui/material/InputLabel';
 import FormControl from '@mui/material/FormControl';
 import Select from '@mui/material/Select';
-
-
-
-
-
-
+import { useState } from 'react';
 
 export default function form() {
+
+
+
+    const [formData, setFormData] = useState({
+        firstName: '',
+        lastName: '',
+        street: '',
+        city: '',
+        state: '',
+        zipCode: '',
+    })
+    const [selectedDate, setSelectedDate] = useState(null);
+    const [department, setDepartment] = useState('');
+    const handleDateChange = (date) => {
+        setSelectedDate(date);
+    };
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        setFormData((prevData) => ({
+            ...prevData,
+            [id]: value,
+        }));
+        setDepartment(event.target.value);
+    }
     const handleSubmit = (event) => {
         event.preventDefault();
-        alert("Employee Created!")
+        console.log('Formulaire soumis :', formData);
     }
-
     return <form onSubmit={handleSubmit} style={{ display: 'flex', flexDirection: 'column' }}>
         <Box className="formField" marginBottom={2}>
             <LocalizationProvider dateAdapter={AdapterDayjs}>
                 <FormGroup>
-                    <TextField label="First Name" id="first-name" variant="outlined" />
-                    <TextField label="Last Name" id="last-name" variant="outlined" />
-                    <DatePicker label="Date of Birth" id="date-of-birth" variant="outlined" />
-                    <DatePicker label="Start Date" id="start-date" variant="outlined" />
+                    <TextField label="First Name" id="firstName" variant="outlined" onChange={handleChange}
+                    />
+                    <TextField label="Last Name" id="lastName" variant="outlined" onChange={handleChange}
+                    />
+                    <DatePicker label="Start Date" id="startDate" variant="outlined" value={selectedDate} onChange={handleDateChange} />
+                    <DatePicker label="Date of Birth" id="dateOfBirth" variant="outlined" value={selectedDate} onChange={handleDateChange} />
                     <Card variant="outlined">
                         <CardContent>
                             <Typography variant="h6" gutterBottom>
                                 Address
                             </Typography>
-
-                            <TextField label="Street" id="street" variant="outlined" />
-                            <TextField label="City" id="city" variant="outlined" />
+                            <TextField label="Street" id="street" variant="outlined" value={formData.street} onChange={handleChange} />
+                            <TextField label="City" id="city" variant="outlined" value={formData.city} onChange={handleChange} />
                             <FormControl fullWidth>
-                                <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                                <InputLabel id="state-select-label">State</InputLabel>
                                 <Select
-                                    labelId="demo-simple-select-label"
-                                    id="demo-simple-select"
-                                    label="Age"
+                                    labelId="state-select-label"
+                                    id="state"
+                                    label="State"
+                                    onChange={handleChange}
                                 >
-                                    {
-                                        //map sur tableau state
-                                    }
+                                    {states.map((state) => (
+                                        <MenuItem key={state.abbreviation} value={state.abbreviation} >
+                                            {state.name}
+                                        </MenuItem>
+                                    ))}
                                 </Select>
                             </FormControl>
-
-                            <TextField label="Zip Code" id="zip-code" variant="outlined" />
+                            <TextField label="Zip Code" id="zipCode" variant="outlined" value={formData.zipCode} onChange={handleChange} />
                         </CardContent>
-
                     </Card>
                     <FormControl fullWidth>
-                        <InputLabel id="demo-simple-select-label">Age</InputLabel>
+                        <InputLabel id="department-select-label">Departement</InputLabel>
                         <Select
-                            labelId="demo-simple-select-label"
-                            id="demo-simple-select"
-                            label="Age"
-                        >
+                            labelId="department-select-label"
+                            id="department"
+                            label="Department"
+                            value={department}
+                            onChange={handleChange}                        >
                             <MenuItem value="Sales">Sales</MenuItem>
                             <MenuItem value="Marketing">Marketing</MenuItem>
                             <MenuItem value="Engineering">Engineering</MenuItem>
@@ -75,9 +96,6 @@ export default function form() {
                             <MenuItem value="Legal">Legal</MenuItem>
                         </Select>
                     </FormControl>
-
-
-
                     <Button className="saveBtn" variant="contained" color="primary" type="submit">
                         Save
                     </Button>
@@ -85,6 +103,5 @@ export default function form() {
                 </FormGroup>
             </LocalizationProvider>
         </Box>
-
     </form >
 }

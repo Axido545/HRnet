25b8e3f89c602055d3bb4f57694752c683ea8a6f
axido545 <axido545@gmail.com>
2024-02-02 19:54:30 +0100

    update header, nav,

--- src/layout/header/header.jsx
+++ src/layout/header/header.jsx
@@ -1,12 +1,12 @@
 import "./header.css"
+import Nav from "../nav/nav"
 import { Link } from "react-router-dom"
 
 
 export default function Header() {
     return <header>
-        <logo>HRnet</logo>
-        <nav>
-            <Link to="/employee-list">View Current Employees</Link>
-        </nav>
-    </header>
+        <Link to="/"> <logo>HR<span>net</span></logo></Link>
+
+        <Nav />
+    </header >
 }
---
+++ src/layout/nav/nav.jsx
@@ -0,0 +1,12 @@
+import "./nav.css"
+import { Link } from "react-router-dom"
+
+export default function Nav() {
+    return <nav>
+        <ul>
+            <li><Link to="/">home</Link></li>
+            <li> <Link to="/employee-list">View Current Employees</Link></li>
+
+        </ul>
+    </nav>
+}
